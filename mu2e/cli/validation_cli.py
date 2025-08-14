1b74ab74cdd2891ca8246590eb31a50f1a43ecab
annabelleboots <annabelle.boots@gmail.com>
2025-08-14 10:50:50 -0500

    Update validation_cli.py

--- mu2e/cli/validation_cli.py
+++ mu2e/cli/validation_cli.py
@@ -14,15 +14,15 @@ def check_retrieval(collection: str = "default", test_zeros: bool=False):
     val.save_retrieval(collection)
     
 
-def chATLAS_generate(filename: str = "chATLAS_questions"):
+def chATLAS_generate(filename: str = "chATLAS_questions", num=None):
     val = validation.BenchmarkGenerator()
-    val.chATLAS_generate_qa_pair()
+    val.chATLAS_generate_qa_pair(num=num)
     val.save(filename)
     
 
-def check_chATLAS(collection: str = "default"):
+def check_chATLAS(collection: str = "default", question_num: int=1):
     val = validation.BenchmarkGenerator()
-    val.check_chATLAS(collection)
+    val.check_chATLAS(collection, question_num=question_num)
     val.save_retrieval(collection, filename="chATLAS_benchmark_scores")
 
     
@@ -36,6 +36,8 @@ def main():
         
     chATLAS_parser = subparsers.add_parser('generate-chATLAS', help='Generate dataset from chATLAS system prompt')
     chATLAS_parser.add_argument("--filename", default="chATLAS_questions", help='Specify chATLAS output filename')
+    chATLAS_parser.add_argument("--num", type=int, default=100, help='Number of questions to be generated') 
+
 
     test_parser = subparsers.add_parser('test-retrieval', help ='Evaluate the dataset')
     test_parser.add_argument(
@@ -46,14 +48,41 @@ def main():
         )
     test_parser.add_argument('--test-zeros', action='store_true', help='Test mc for retrievals that score 0')
 
+    chATLAS_test_parser = subparsers.add_parser('test-chATLAS', help='Test qa pairs from chATLAS questions')
+    
+
+    chATLAS_test_parser.add_argument(
+        "--rigor",
+        type=str,
+        choices=["early_career", "established_worker", "experienced_professional"],
+        default="early_career",
+        help="Determine which difficulty of question to use"
+    )
+
+    chATLAS_test_parser.add_argument(
+            '--collection',
+            type=str,     
+            default='default',    
+            help='Specify a collection (default if not given)'
+        )
+
+    
     args = parser.parse_args()
 
     if args.command == 'generate':
         generate(filename=args.filename, num=args.num)
     elif args.command == 'generate-chATLAS':
-        chATLAS_generate(filename=args.filename)
+        chATLAS_generate(filename=args.filename, num=args.num)
     elif args.command == 'test-retrieval':
         check_retrieval(args.collection, test_zeros=args.test_zeros)
+    elif args.command == 'test-chATLAS':
+        rigor_map = {
+            "early_career": 0,
+            "established_worker": 1, 
+            "experienced_professional": 2
+        }
+        check_chATLAS(args.collection, question_num=rigor_map[args.rigor])
+
         
 
 if __name__ == "__main__":
